73101afef8ead9836befb4eb802df3e221a5bd97
Kanan Hajiyev <haciyevk785@gmail.com>
2023-05-08 16:26:21 +0400

    perf(changeDetects): onPush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent{
   isVisible!:boolean;
   timer!: any
+
+  constructor(private cdRef: ChangeDetectorRef) {
+  }
 
   showIcon() {
     this.isVisible = true
@@ -15,6 +19,7 @@ export class AppComponent{
     clearInterval(this.timer)
     this.timer = setInterval(() => {
       this.isVisible = false
+      this.cdRef.detectChanges()
     }, 3000)
   }
 }
